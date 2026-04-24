1750379278b4244933b151ffcbdb793fee3c20d7
ujjawal396 <ujjawaljha08@gmail.com>
2026-04-24 23:32:55 +0530

    Added get notifications api

--- services/notification.service.js
+++ services/notification.service.js
@@ -1,5 +1,7 @@
 const Ticket = require('../models/ticketNotification');
 const { STATUS } = require('../utils/constants');
+
+
 const create = async (data) => {
     try {
         const ticket = await Ticket.create(data);
@@ -19,6 +21,33 @@ const create = async (data) => {
     }
 }
 
+const getAll = async () => {
+    try {
+        const response = await Ticket.find();
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const getById = async (id) => {
+    try {
+        const response = await Ticket.findById(id);
+        if(!response) {
+            throw {
+                err: 'No ticket details found',
+                code: STATUS.NOT_FOUND
+            };
+        }
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
 module.exports = {
-    create
+    create,
+    getAll,
+    getById,
 }
